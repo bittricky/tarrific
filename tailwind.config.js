95c89b18675843b05547ed7cb9f34a1fa7135eac
Mitul Patel <me@mitulpa.tel>
2025-03-27 11:09:10 -0400

    feat(config): added color scheme to the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#E53E3E", // red-600
+          50: "#FEF2F2",
+          100: "#FEE2E2",
+          200: "#FECACA",
+          300: "#FCA5A5",
+          400: "#F87171",
+          500: "#EF4444",
+          600: "#E53E3E",
+          700: "#DC2626",
+          800: "#B91C1C",
+          900: "#991B1B",
+          950: "#7F1D1D",
+        },
+        secondary: {
+          DEFAULT: "#EAB308", // yellow-500
+          50: "#FEFCE8",
+          100: "#FEF9C3",
+          200: "#FEF08A",
+          300: "#FDE047",
+          400: "#FACC15",
+          500: "#EAB308",
+          600: "#CA8A04",
+          700: "#A16207",
+          800: "#854D0E",
+          900: "#713F12",
+          950: "#422006",
+        },
+      },
+    },
   },
   plugins: [],
 };
